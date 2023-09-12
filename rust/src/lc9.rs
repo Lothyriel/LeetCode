30a65cd6a03c3d4c5105cd6934c97adcc6f0097e
fastjonh <fastjonh@gmail.com>
2023-09-12 02:03:40 -0300

    9: palindrome

---
+++ rust/src/lc9.rs
@@ -0,0 +1,19 @@
+pub fn is_palindrome(x: i32) -> bool {
+    let x = x.to_string();
+
+    x.bytes().zip(x.bytes().rev()).all(|(a, b)| a == b)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Not;
+
+    use crate::lc9::is_palindrome;
+
+    #[test]
+    fn tests() {
+        assert!(is_palindrome(121));
+        assert!(is_palindrome(-121));
+        assert!(is_palindrome(10).not());
+    }
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,3 +2,4 @@ pub mod lc1;
 pub mod lc1456;
 pub mod lc1736;
 pub mod lc239;
+pub mod lc9;
