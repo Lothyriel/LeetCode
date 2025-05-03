e16a7f9f80e63d9a6ced3bf2cee52e0375fe8fc7
João Xavier <fastjonh@gmail.com>
2025-05-03 14:03:21 -0300

    lc 242: benchmarking array vs map solutions

---
+++ rust/benches/lc0242.rs
@@ -0,0 +1,32 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use leetcode::lc0242::{is_anagram_array, is_anagram_map};
+
+fn is_anagram(c: &mut Criterion) {
+    c.bench_function("is_anagram map small", |b| {
+        b.iter(|| is_anagram_map("listen".to_string(), "silent".to_string()))
+    });
+
+    c.bench_function("is_anagram map large", |b| {
+        let s = "abcdefghijklmnopqrstuvwxyz".repeat(1000);
+
+        let t: String = s.chars().rev().collect();
+
+        b.iter(|| is_anagram_map(s.clone(), t.clone()))
+    });
+
+    c.bench_function("is_anagram array small", |b| {
+        b.iter(|| is_anagram_array("listen".to_string(), "silent".to_string()))
+    });
+
+    c.bench_function("is_anagram array large", |b| {
+        let s = "abcdefghijklmnopqrstuvwxyz".repeat(1000);
+
+        let t: String = s.chars().rev().collect();
+
+        b.iter(|| is_anagram_array(s.clone(), t.clone()))
+    });
+}
+
+criterion_group!(benches, is_anagram);
+criterion_main!(benches);
--- rust/src/lc0242.rs
+++ rust/src/lc0242.rs
@@ -1,7 +1,22 @@
 use std::collections::HashMap;
 
-pub fn is_anagram(s: String, t: String) -> bool {
+pub fn is_anagram_map(s: String, t: String) -> bool {
     get_char_freq(&s) == get_char_freq(&t)
+}
+
+pub fn is_anagram_array(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+
+    let mut freq = [0; 26];
+
+    for (a, b) in s.bytes().zip(t.bytes()) {
+        freq[(a - b'a') as usize] += 1;
+        freq[(b - b'a') as usize] -= 1;
+    }
+
+    freq.iter().all(|&x| x == 0)
 }
 
 fn get_char_freq(s: &str) -> HashMap<u8, u16> {
@@ -10,4 +25,3 @@ fn get_char_freq(s: &str) -> HashMap<u8, u16> {
         acc
     })
 }
-
