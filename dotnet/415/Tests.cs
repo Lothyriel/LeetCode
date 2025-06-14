32662fd88251f923819d64272c279af13aa007b3
João Xavier <fastjonh@gmail.com>
2025-06-14 08:30:36 -0300

    adjusting AddStrings folder name
    also created a csproj and some tests

---
+++ dotnet/415/Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace _415;
+
+public class Tests
+{
+  [Test]
+  [TestCase("11", "123", "134")]
+  [TestCase("456", "77", "533")]
+  [TestCase("0", "0", "0")]
+  public void Test1(string num1, string num2, string expected)
+  {
+    Assert.That(Solution.AddStrings(num1, num2), Is.EqualTo(expected));
+  }
+}
