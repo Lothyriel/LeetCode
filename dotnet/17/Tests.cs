02a92a4dd75a2f4c4d28078e933db86c9679a9da
João Xavier <fastjonh@gmail.com>
2025-06-14 10:14:19 -0300

    lc 17 - dotnet
    ok this was way harder than I thought... my excuse is that I need to
    sleep

---
+++ dotnet/17/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace _17;
+
+public class Tests
+{
+  [Test]
+  public void Test_23()
+  {
+    List<string> expected = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
+
+    var result = Solution.LetterCombinations("23");
+
+    Assert.That(result, Is.EqualTo(expected));
+  }
+
+  [Test]
+  public void Test_empty()
+  {
+    List<string> expected = [];
+
+    var result = Solution.LetterCombinations("");
+
+    Assert.That(result, Is.EqualTo(expected));
+  }
+}
