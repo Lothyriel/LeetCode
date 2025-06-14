53b6cd096d37436cc6bb0398ba2231bbfedf18d6
João Xavier <fastjonh@gmail.com>
2025-06-14 11:41:13 -0300

    lc 22 - dotnet
    im cooked
    
    vota vota e confirma

---
+++ dotnet/22/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace _22;
+
+public class Tests
+{
+  [Test]
+  public void Test_1()
+  {
+    List<string> expected = ["()"];
+
+    var result = Solution.GenerateParenthesis(1);
+
+    Assert.That(result, Is.EqualTo(expected));
+  }
+
+  [Test]
+  public void Test_3()
+  {
+    List<string> expected = ["((()))","(()())","(())()","()(())","()()()"];
+
+    var result = Solution.GenerateParenthesis(3);
+
+    Assert.That(result, Is.EqualTo(expected));
+  }
+}
