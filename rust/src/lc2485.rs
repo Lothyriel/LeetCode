bd43a22585907eda924a5f3e91cb6c11c91781b4
João Xavier <fastjonh@gmail.com>
2026-05-24 15:32:30 -0300

    lc 2485 the correct way

--- rust/src/lc2485.rs
+++ rust/src/lc2485.rs
@@ -1,35 +1,9 @@
 pub fn pivot_integer(n: i32) -> i32 {
-    if n == 1 {
-        return 1;
-    }
+    let total = n * (n + 1) / 2;
 
-    if n == 696 {
-        return -1;
-    }
+    let root = total.isqrt();
 
-    let mut start = 0;
-    let mut end = n;
-
-    let mut left = 0;
-    let mut right = 0;
-    let mut count = 0;
-
-    loop {
-        count += 1;
-
-        if right <= left {
-            right += end;
-            end -= 1;
-        }
-        if left <= right {
-            left += start;
-            start += 1;
-        }
-
-        if start >= end {
-            return if left == right { count } else { -1 };
-        }
-    }
+    if root * root == total { root } else { -1 }
 }
 
 #[cfg(test)]
