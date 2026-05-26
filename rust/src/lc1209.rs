8c76eb20744d4317c15f7b85acd4784f4c3f225c
João Xavier <fastjonh@gmail.com>
2026-05-26 14:58:21 -0300

    lc 1209: fix logic bugs
    We were almost there...

--- rust/src/lc1209.rs
+++ rust/src/lc1209.rs
@@ -5,29 +5,32 @@ pub fn remove_duplicates(s: String, k: i32) -> String {
 }
 
 fn candy_crush(mut column: Vec<char>, k: usize) -> (Vec<char>, usize) {
-    let mut current_count = 0;
-    let mut current_candy = column[0];
-    let mut start = 0;
     let mut groups_removed = 0;
 
     loop {
-        for &candy in &column {
+        let mut current_candy = column[0];
+        let mut current_count = 1;
+        let mut start = 0;
+        let mut found = false;
+
+        for i in 1..column.len() {
+            let candy = column[i];
             if current_candy == candy {
                 current_count += 1;
             } else if current_count >= k {
+                found = true;
                 break;
             } else {
                 current_candy = candy;
-                current_count = 0;
-                start += current_count;
+                start = i;
+                current_count = 1;
             }
         }
 
-        column = get_new_state(column, start, current_count);
-
-        if current_count < k {
+        if !found {
             return (column, groups_removed);
         } else {
+            column = get_new_state(column, start, current_count);
             groups_removed += 1;
         }
     }
@@ -39,7 +42,7 @@ fn get_new_state(column: Vec<char>, start: usize, current_count: usize) -> Vec<c
     for i in 0..column.len() {
         let candy = column[i];
 
-        if i > start && i < start + current_count {
+        if i >= start && i < start + current_count {
             continue;
         }
 
